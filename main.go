b63ec96e06a862032855a7c12f97b276f79c6755
Akbarshoh Ismoilov <akbarshohismoilov001@gmail.com>
2022-06-13 10:40:16 +0500

    commited on goland

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	
 	fmt.Print("Some")
+	// ajslkdfjeroigj
+	// dskjg
+	// ejkgerio
+	// djgqero
+
+	// sdjg
+	// iuhgasdklvjv
+	// dsjhgoierqgdsv
+	// dsjhgoierqgdsvjdsg
 }
